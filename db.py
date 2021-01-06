ea175c1f2c145668482f58f4d406e472c3b9478a
robgeb <robin.van.sabian@gmail.com>
2021-01-06 13:19:01 +0100

    user2 angelegt

--- db.py
+++ db.py
@@ -1,10 +1,16 @@
 import pymongo
 from pymongo import MongoClient
 
-cluster = MongoClient("mongodb+srv://user1:TAMAtama12.@cluster0.hin53.mongodb.net/test")
+cluster = MongoClient("mongodb+srv://user2:PJS2021@cluster0.hin53.mongodb.net/test")
 db = cluster["Testdaten"]
 collection = db["customers"]
 
-post = {"_id":2, "name":"Robin", "score":1}
+
+#zum Testen des Codes ID anpassen
+#Robin: 1-10
+#nico: 11-20
+#Bene: 21-30
+#Alpi: 31-40
+post = {"_id":5, "name":"Robin", "score":1}
 
 collection.insert_one(post)
