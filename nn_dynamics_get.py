eebc5f4cd60788c3e1086dacbaae3d4d79340999
Nico93-n <nb.neis@t-online.de>
2021-01-24 13:29:35 +0100

    frfr

--- nn_dynamics_get.py
+++ nn_dynamics_get.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://10.105.11.42:7048/BC140/api/v1.0/" #add 'items' für die Artikelabfrage
+url = "http://10.105.11.42:7048/BC140/api/v1.0/items"   #'items' für die Artikelabfrage
 
 payload={}
 headers = {
