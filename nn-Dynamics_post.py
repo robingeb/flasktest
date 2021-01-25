872104b13a63a5cb383e048fd8e082cefec707dc
Nico93-n <nb.neis@t-online.de>
2021-01-25 16:45:35 +0100

    gtgrgtr

--- nn-Dynamics_post.py
+++ nn-Dynamics_post.py
@@ -5,30 +5,18 @@ import requests
 
 url = "http://10.105.11.42:7048/BC140/api/v1.0/items"
 
-payload="""
-{
-  "number": "1896-S",
-  "displayName": "ATHENS Desk",
-  "type": "Inventory",
-  "blocked": false,
-  "baseUnitOfMeasure": {
-    "unitCode": "PCS",
-    "unitName": "Piece",
-    "symbol": "",
-    "unitConversion": null
-  },
-  "gtin": "",
-  "itemCategory": {
-    "categoryId": "TABLE", 
-    "description": "Assorted Tables"
-  },
-  "inventory": 0,
-  "unitPrice": 1000.8,
-  "priceIncludesTax": false,
-  "unitCost": 780.7,
-  "taxGroupCode": "FURNITURE"
-}"""
-#"{\r\n  \"number\": \"1896-S\",\r\n  \"displayName\": \"ATHENS Desk\",\r\n  \"type\": \"Inventory\",\r\n  \"blocked\": false,\r\n  \"baseUnitOfMeasure\": {\r\n    \"unitCode\": \"PCS\",\r\n    \"unitName\": \"Piece\",\r\n    \"symbol\": \"\",\r\n    \"unitConversion\": null\r\n  },\r\n  \"gtin\": \"\",\r\n  \"itemCategory\": {\r\n    \"categoryId\": \"TABLE\", \r\n    \"description\": \"Assorted Tables\"\r\n  },\r\n  \"inventory\": 0,\r\n  \"unitPrice\": 1000.8,\r\n  \"priceIncludesTax\": false,\r\n  \"unitCost\": 780.7,\r\n  \"taxGroupCode\": \"FURNITURE\"\r\n} "
+payload= "{\r\n  \"number\": \"100nico\",\r\n  \"displayName\": \"ATHENS Desk\",\r\n  \"type\": \"Inventory\",\r\n  \"blocked\": false,\r\n  \"gtin\": \"\",\r\n  \"inventory\": 0,\r\n  \"unitPrice\": 1000.8,\r\n  \"priceIncludesTax\": false,\r\n  \"unitCost\": 780.7\r\n} "
+# {
+#   "number": "1896-S",
+#   "displayName": "ATHENS Desk",
+#   "type": "Inventory",
+#   "blocked": false,
+#   "gtin": "",
+#   "inventory": 0,
+#   "unitPrice": 1000.8,
+#   "priceIncludesTax": false,
+#   "unitCost": 780.7
+# } 
 headers = {
   'Authorization': 'Basic V0lJTkZccm9iaW4uZ2ViaGFyZHQ6a2lCVEVLTnFaVzYyN24zQXl1TkQ0YzJFdVpwQkZJM3dLZE9OcXlaa2JXbz0=',
   'Content-Type': 'application/json'
