7bfffa90e2bb640d870aea91520c2cfa7fbf2eb5
bene401 <31989084+bene401@users.noreply.github.com>
2021-01-25 09:43:43 +0100

    OpenZ

---
+++ hb_openz.py
@@ -0,0 +1,3 @@
+# Es konnte keine Api für den Import von MaschinenDaten gefunden werden. Es existiert ausschließlich eine API für den E-Commerce (Lieferantenrechnung, Einkaufsaufträge)
+# siehe: https://openz.de/handbuch/open-source-erp-einstellungen/konfiguration-reportvorlagen/
+# Es existiert in OpenZ eine eigene Maschinenverwaltung, für die aber kein API Zugriff existiert.
