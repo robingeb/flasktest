bca7a77dce59f139fcfb7050347ad456b2fedc6e
Alpi <57751202+Alpi37@users.noreply.github.com>
2021-02-01 08:52:04 +0100

    ay12

---
+++ ay_weclapp_flask.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, make_response, jsonify, json
+import requests
+import json
+from requests.auth import HTTPDigestAuth
+
+
+#Erstellt neue Apllikationi
+app = Flask(__name__)
+
+@app.route('/')
+def homepage(): 
+    url = "https://wwmeqaovgkvqrzk.weclapp.com/webapp/api/v1/article"
+    #payload = {}
+    headers = {
+    'AuthenticationToken': '8e98b02b-eb0e-4ea8-a443-9d8dcada588f'
+    }
+
+    response = requests.request("GET", url, headers=headers) #data = payload
+    data = json.loads(response.text)
+    return jsonify(data)  
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+
+
+
