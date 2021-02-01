98468a6bf88f468c4653865ad2b2080883cd650e
Alpi <57751202+Alpi37@users.noreply.github.com>
2021-02-01 08:43:05 +0100

    ay11

--- ay_xentral_flask.py
+++ ay_xentral_flask.py
@@ -1,4 +1,3 @@
-import xmltodict
 from flask import Flask, request, make_response, jsonify, json
 import requests
 import json
@@ -12,13 +11,14 @@ app = Flask(__name__)
 @app.route('/')
 def homepage(): 
     url = "http://132.187.226.135/www/api//index.php/v1/artikel" 
-    headers = {
-    'Authorization': 'Basic V0lJTkZccm9iaW4uZ2ViaGFyZHQ6a2lCVEVLTnFaVzYyN24zQXl1TkQ0YzJFdVpwQkZJM3dLZE9OcXlaa2JXbz0='
+    auth = {
+    'username': 'Alpi',
+    "password": "Alpi"
     }
 
-    response = requests.request("GET", url, auth=HTTPDigestAuth("Robin", "Robin")) #filtern nach "name_de oder "name_en"
+    response = requests.request("GET", url, auth=HTTPDigestAuth(auth['username'], auth['password'])) #filtern nach "name_de oder "name_en"
     data = json.loads(response.text)
     return jsonify({"Result":data}) 
-
+  
 if __name__ == '__main__':
   app.run(debug=True)
