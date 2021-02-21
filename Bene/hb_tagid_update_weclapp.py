2788023fa3d01fdd1d90c9e4c09ab1d8bfd5e911
bene401 <31989084+bene401@users.noreply.github.com>
2021-02-21 13:42:39 +0100

    update Funktion

--- Bene/hb_tagid_update_weclapp.py
+++ Bene/hb_tagid_update_weclapp.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import json
+import pymongo
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from hb_weclapp import *
 
 # Ziel ändern des Namens von "Standard-Schlinge 1A" auf "Standard-Schlinge 1AA"
@@ -12,35 +15,57 @@ def main():
     auth = {
     'AuthenticationToken': '837196b1-b252-4bc2-98e4-d7a4f9250a43'
     }
+
+    # MongoDB
+    # app.config["MONGO_URI"] = "mongodb+srv://user2:PJS2021@cluster0.hin53.mongodb.net/test"
+    # mongo = PyMongo(app)
+    client = MongoClient("mongodb+srv://user2:PJS2021@cluster0.hin53.mongodb.net/test")
     
     #Instatiate WeClapp-API
     weClappAPI = WeClappAPI(url, auth)
     
     # Inventar und Device Liste von TagIdeasy erhalten
-    inventar, device = get_tagideasy()
+    # inventar, device = get_tagideasy()
+    inventar = get_tagideasy(client)
 
     # Liste von Ids von Instanzen im Inventar: [Seriennummer, Artikelnummer, index in inventar-list]
-    tagIdeasy_ids = [[instance["core"]["serial_number"], instance["core"]["articel_id_buyer"], inventar["results"].index(instance) ] for instance in inventar["results"]]
+    #tagIdeasy_ids = [[instance["core"]["serial_number"], instance["core"]["articel_id_buyer"], inventar["results"].index(instance) ] for instance in inventar["results"]]
+    tagIdeasy_ids = []
 
     # Relevante Artikel (Geräte) aus WeClapp laden (get-Request)
-    weclapp_ids, weclapp_instances = get_articel(url,auth, tagIdeasy_ids, device, weClappAPI)
+    #weclapp_ids, weclapp_instances = get_articel(url,auth, tagIdeasy_ids, device, weClappAPI)
 
     # zu übertragende Werte aus TagIdeasy zu Weclapp Artikeln zuordnen und diese aktualisiert als Dataframe ausgeben
-    df_mapped = map_attributes(weclapp_ids, weclapp_instances, device, inventar)
+    #df_mapped = map_attributes(weclapp_ids, weclapp_instances, device, inventar)
 
     # aktualisierte Artikel in Weclapp updaten (put-Request)
-    update_articel(df_mapped, weClappAPI)
+    #update_articel(df_mapped, weClappAPI)
     #print(weclapp_ids)
 
 
 #1) Get Artikel von TagIdeasy, welche geändert werden sollen
-def get_tagideasy():
+def get_tagideasy(client_mongo):
     #TODO: Mit Prüfmanagementdummy verknüpfen
-    with open("1_example_inventar.json") as f:
-        inventar = json.loads(f.read()) 
-    with open("1_example_geraete.json") as f:
-        device = json.loads(f.read())
-    return inventar, device
+    # with open("1_example_inventar.json") as f:
+    #     inventar = json.loads(f.read()) 
+    # with open("1_example_geraete.json") as f:
+    #     device = json.loads(f.read())
+    # return inventar, device
+
+    # Collection "Prüfberichte" in MongoDB auswählen
+    db = client_mongo['Prüfberichte']
+    col = db['Prüfberichte']
+    #daten = col.find_one({"Artikelnummer": "002"})
+
+    # Alle Prüfberichte erhalten, welche Zeit dem letzten Update von WeClapp erstellt worden sind
+    # TODO: Abbruchfunktion, wenn keine Prüfberichte geladen werden konnten
+    daten = []
+    last_update = 0
+    for doc in col.find({"Datum":{"$gt":last_update}}):
+        daten.append(doc)    
+    print(daten)
+    #return daten
+    
 
 #2) Get-Request der Artikel um die ids der zu updatenden Geräte zu erhalten
 def get_articel(url, auth, ids, device, weClappAPI ):    
@@ -88,6 +113,7 @@ def map_attributes(article_ids, instance_weclapp, device, inventar):
         # Beispielhaftes ändern des Namens und der Id
         df_mapping._set_value(ids[4], "name",  device["results"][ids[3]]["core"]["device_name"]) 
         df_mapping._set_value(ids[4], "articleNumber", ids[0] )
+       
         
     # print(df_mapping)
     print(df_mapping[["name", "articleNumber"]])
